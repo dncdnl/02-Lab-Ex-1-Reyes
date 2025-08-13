2b61fc7d3c01074c4979d6a4414fb4773fd90127
dncdnl <danicadaniela.cesicar@gmail.com>
2025-08-13 19:26:07 +0800

    creating accessor

--- CalculatorApplication/CalculatorClass.cs
+++ CalculatorApplication/CalculatorClass.cs
@@ -7,9 +7,19 @@ using System.Threading.Tasks;
 namespace CalculatorApplication
 {
     // Declaring a generic Formula
-    public delegate X Formula<X>(X arg1);
+    public delegate X Formula<X>(X arg1, X arg2);
     internal class CalculatorClass
     {
+        //setting variable and data type
         public Formula<double> formula;
     }
-}
+        //Method creation
+    public static double getSum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public static double getDifference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
